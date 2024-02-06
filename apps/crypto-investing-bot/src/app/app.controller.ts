28e0e7abcf8910c060c13212c24b1aaac4944d6c
Andrei Mazniak <andreymaznyak@gmail.com>
2024-02-06 22:48:32 +0700

    clean code

--- apps/crypto-investing-bot/src/app/app.controller.ts
+++ apps/crypto-investing-bot/src/app/app.controller.ts
@@ -1,14 +1,9 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { OutgoingMessage } from 'http';
-import { ZerionApiService } from './zerion-api.service';
-import { XlsxService } from './xlsx.service';
-type Response = OutgoingMessage & { set: (data: unknown) => unknown, send: (data: unknown) => unknown };
+
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
