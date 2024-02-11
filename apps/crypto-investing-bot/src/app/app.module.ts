a7e48a758371df8fa6a4ebad16b856b8bae19217
s-s-chernov <s-s-chernov@yandex-team.ru>
2024-02-11 14:11:00 +0300

    add google sheets user

--- apps/crypto-investing-bot/src/app/app.module.ts
+++ apps/crypto-investing-bot/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppConfigModule } from './app.config';
 import { ZerionApiService } from './zerion-api.service';
 import { XlsxService } from './xlsx.service';
 import { TelegramBotService } from './telegram-bot.service';
+import { GoogleSheetModule } from 'nest-google-sheet-connector';
+import { GS_CREDENTIALS } from './google-sheet/credentials.consts';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, GoogleSheetModule.register(GS_CREDENTIALS)],
   controllers: [AppController],
   providers: [AppService, ZerionApiService, XlsxService, TelegramBotService],
 })
