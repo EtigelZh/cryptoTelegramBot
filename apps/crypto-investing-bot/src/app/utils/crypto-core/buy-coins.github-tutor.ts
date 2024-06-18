11f5f13902e69c0fd72436a05ffd528a9dc06247
Andrei Mazniak <andreymaznyak@gmail.com>
2024-06-18 23:57:32 +0300

    feat: add supporting swap token with ETH #35

--- apps/crypto-investing-bot/src/app/utils/crypto-core/buy-coins.github-tutor.ts
+++ apps/crypto-investing-bot/src/app/utils/crypto-core/buy-coins.github-tutor.ts
@@ -16,8 +16,14 @@ import { BigNumber } from '@ethersproject/bignumber';
 import ERC20_abi from "./ERC20-abi.json"
 import { resolve } from "path";
 
+const WETH_ADDRESS_NETWORK_MAP = {
+    [1]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    [42161]: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'
+};
+
 const defaultEnvPath = resolve(__dirname, '.env');
 dotenv.config({ path: defaultEnvPath })
+
 async function main() {
     const options = await yargs(hideBin(process.argv))
         .usage("Swaps tokens, based on Uniswap V3 SDK")
@@ -28,7 +34,6 @@ async function main() {
         .option("amount-in", { describe: "Input token amount to swap", type: "string", demandOption: true })
         .argv;
 
-
     const chainId = parseInt(options.chainId);  // chainId must be integer
     const walletAddress = options.walletAddress;
     const tokenInContractAddress = options.tokenInAddress;
@@ -36,26 +41,11 @@ async function main() {
     const inAmountStr = options.amountIn;
     const { API_URL = '', PRIVATE_KEY = '' } = process.env;
     console.log(`API_URL: ${API_URL}`);
-    // ============= PART 1 --- connect to blockchain and get token balances
     console.log("Connecting to blockchain, loading token balances...");
     console.log('');
 
-    // Ethers.js provider to access blockchain
-    // As we're using Alchemy, it is JsonRpcProvider
-    // In case of React app + MetaMask it should be initialized as "new ethers.providers.Web3Provider(window.ethereum);"
     const provider = new ethers.providers.AlchemyProvider(chainId, API_URL);
-
-
     const signer = new ethers.Wallet(PRIVATE_KEY!, provider);
-
-    // // In case of React + Metamask it should be initialized as "provider.getSigner();"
-    // // as we already have signer provided by Metamask
-    // var signer = wallet.provider.getSigner(wallet.address);
-
-
-    // create token contracts and related objects
-    const contractIn = new ethers.Contract(tokenInContractAddress, ERC20_abi, signer);
-    const contractOut = new ethers.Contract(tokenOutContractAddress, ERC20_abi, signer);
 
     const getTokenAndBalance = async function (contract: ethers.Contract) {
         const [dec, symbol, name, balance] = await Promise.all(
@@ -65,40 +55,55 @@ async function main() {
                 contract.name(),
                 contract.balanceOf(walletAddress)
             ]);
-
         return [new Token(chainId, contract.address, dec, symbol, name), balance];
     }
 
-    const [tokenIn, balanceTokenIn] = await getTokenAndBalance(contractIn);
-    const [tokenOut, balanceTokenOut] = await getTokenAndBalance(contractOut);
+    const WETH_ADDRESS = WETH_ADDRESS_NETWORK_MAP[+chainId]; // WETH address on mainnet, replace with appropriate address for other chains
+    if (!WETH_ADDRESS) {
+        throw new Error(`WETH address is not defined for chainId ${chainId}`);
+    }
+    const isETH = (address: string) => address.toLowerCase() === ethers.constants.AddressZero.toLowerCase();
+
+    let tokenIn, tokenOut, balanceTokenIn, balanceTokenOut, tokenOutContract, tokenInContract;
+    if (isETH(tokenInContractAddress)) {
+        tokenOutContract = new ethers.Contract(tokenOutContractAddress, ERC20_abi, signer);
+        [tokenOut, balanceTokenOut] = await getTokenAndBalance(tokenOutContract);
+        tokenIn = new Token(chainId, WETH_ADDRESS, 18, "WETH", "Wrapped Ether");
+        balanceTokenIn = await provider.getBalance(walletAddress);
+    } else if (isETH(tokenOutContractAddress)) {
+        tokenInContract = new ethers.Contract(tokenInContractAddress, ERC20_abi, signer);
+        [tokenIn, balanceTokenIn] = await getTokenAndBalance(tokenInContract);
+        tokenOut = new Token(chainId, WETH_ADDRESS, 18, "WETH", "Wrapped Ether");
+        balanceTokenOut = await provider.getBalance(walletAddress);
+    } else {
+        tokenInContract = new ethers.Contract(tokenInContractAddress, ERC20_abi, signer);
+        tokenOutContract = new ethers.Contract(tokenOutContractAddress, ERC20_abi, signer);
+        [tokenIn, balanceTokenIn] = await getTokenAndBalance(tokenInContract);
+        [tokenOut, balanceTokenOut] = await getTokenAndBalance(tokenOutContract);
+    }
 
     console.log(`Wallet ${walletAddress} balances:`);
     console.log(`   Input: ${tokenIn.symbol} (${tokenIn.name}): ${ethers.utils.formatUnits(balanceTokenIn, tokenIn.decimals)}`);
     console.log(`   Output: ${tokenOut.symbol} (${tokenOut.name}): ${ethers.utils.formatUnits(balanceTokenOut, tokenOut.decimals)}`);
     console.log("");
 
-    // ============= PART 2 --- get Uniswap pool for pair TokenIn-TokenOut
     console.log("Loading pool information...");
 
-    // this is Uniswap factory, same address on all chains
-    // (from https://docs.uniswap.org/protocol/reference/deployments)
     const UNISWAP_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
     const factoryContract = new ethers.Contract(UNISWAP_FACTORY_ADDRESS, IUniswapV3Factory.abi, provider);
 
-    // loading pool smart contract address
     const poolAddress = await factoryContract.getPool(
         tokenIn.address,
         tokenOut.address,
-        3000);  // commission - 3%
+        3000);
 
-    if (Number(poolAddress).toString() === "0") // there is no such pool for provided In-Out tokens.
+    if (Number(poolAddress).toString() === "0")
         throw `Error: No pool ${tokenIn.symbol}-${tokenOut.symbol}`;
 
     const poolContract = new ethers.Contract(poolAddress, IUniswapV3Pool.abi, provider);
 
     const getPoolState = async function () {
         const [liquidity, slot] = await Promise.all([poolContract.liquidity(), poolContract.slot0()]);
-
         return {
             liquidity: liquidity,
             sqrtPriceX96: slot[0],
@@ -131,7 +136,6 @@ async function main() {
         }
     }
 
-    // loading immutable pool parameters and its current state (variable parameters)
     const [immutables, state] = await Promise.all([getPoolImmutables(), getPoolState()]);
 
     const pool = new Pool(
@@ -143,20 +147,15 @@ async function main() {
         state.tick
     );
 
-    // print token prices in the pool
     console.log("Token prices in pool:");
     console.log(`   1 ${pool.token0.symbol} = ${pool.token0Price.toSignificant()} ${pool.token1.symbol}`);
     console.log(`   1 ${pool.token1.symbol} = ${pool.token1Price.toSignificant()} ${pool.token0.symbol}`);
     console.log('');
 
-
-    // ============= PART 3 --- Giving a quote for user input
     console.log("Loading up quote for a swap...");
 
     const amountIn = ethers.utils.parseUnits(inAmountStr, tokenIn.decimals);
 
-    // this is Uniswap quoter smart contract, same address on all chains
-    // (from https://docs.uniswap.org/protocol/reference/deployments)
     const UNISWAP_QUOTER_ADDRESS = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'
     const quoterContract = new ethers.Contract(UNISWAP_QUOTER_ADDRESS, QuoterABI.abi, provider);
 
@@ -171,8 +170,6 @@ async function main() {
     console.log(`   You'll get approximately ${ethers.utils.formatUnits(quotedAmountOut, tokenOut.decimals)} ${tokenOut.symbol} for ${inAmountStr} ${tokenIn.symbol}`);
     console.log('');
 
-
-    // ============= PART 4 --- Loading a swap route
     console.log('');
     console.log("Loading a swap route...");
 
@@ -183,23 +180,14 @@ async function main() {
         inAmount,
         tokenOut,
         TradeType.EXACT_INPUT,
-        // swapOptions
         {
             type: SwapType.SWAP_ROUTER_02,
             recipient: walletAddress,
-            slippageTolerance: new Percent(5, 100),          // Big slippage – for a test
-            deadline: Math.floor(Date.now() / 1000 + 1800)    // add 1800 seconds – 30 mins deadline
+            slippageTolerance: new Percent(5, 100),
+            deadline: Math.floor(Date.now() / 1000 + 1800)
         },
-        // router config
         {
-            // only one direct swap for a reason – 2 swaps thru DAI (USDT->DAI->WETH) didn't work on Rinkeby
-            // There was an overflow problem https://rinkeby.etherscan.io/tx/0xaed297f2f51f17b329ce755b11635980268f3fc88aae10e78cf59f2c6e65ca7f
-            // The was DAI balance for UniswapV2Pair was greater than 2^112-1 (https://github.com/Uniswap/v2-core/blob/master/contracts/UniswapV2Pair.sol)
-            // UniswapV2Pair – https://rinkeby.etherscan.io/address/0x8b22f85d0c844cf793690f6d9dfe9f11ddb35449
-            // WETH – https://rinkeby.etherscan.io/address/0xc778417e063141139fce010982780140aa0cd5ab#readContract
-            // DAI – https://rinkeby.etherscan.io/address/0xc7ad46e0b8a400bb3c915120d284aafba8fc4735#readContract (balance of UniswapV2Pair more than 2^112-1)
-
-            maxSwapsPerPath: 1 // remove this if you want multi-hop swaps as well.
+            maxSwapsPerPath: 1
         }
     );
 
@@ -208,7 +196,6 @@ async function main() {
 
     console.log(`   You'll get ${route.quote.toFixed()} of ${tokenOut.symbol}`);
 
-    // output quote minus gas fees
     console.log(`   Gas Adjusted Quote: ${route.quoteGasAdjusted.toFixed()}`);
     console.log(`   Gas Used Quote Token: ${route.estimatedGasUsedQuoteToken.toFixed()}`);
     console.log(`   Gas Used USD: ${route.estimatedGasUsedUSD.toFixed()}`);
@@ -216,37 +203,25 @@ async function main() {
     console.log(`   Gas Price Wei: ${route.gasPriceWei}`);
     console.log('');
 
-    // // ============= PART 5 --- Making actual swap
-    console.log("Approving amount to spend...");
+    if (!isETH(tokenInContractAddress)) {
+        console.log("Approving amount to spend...");
+        const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
+        const approveTxUnsigned = await tokenInContract.populateTransaction.approve(V3_SWAP_ROUTER_ADDRESS, amountIn);
+        approveTxUnsigned.chainId = chainId;
+        approveTxUnsigned.gasLimit = await tokenInContract.estimateGas.approve(V3_SWAP_ROUTER_ADDRESS, amountIn);
+        approveTxUnsigned.gasPrice = await provider.getGasPrice();
+        approveTxUnsigned.nonce = await provider.getTransactionCount(walletAddress);
 
-    // address of a swap router
-    const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
-
-    // For Metamask it will be just "await contractIn.approve(V3_SWAP_ROUTER_ADDRESS, amountIn);"
-
-    // here we just create a transaction object (not sending it to blockchain).
-    const approveTxUnsigned = await contractIn.populateTransaction.approve(V3_SWAP_ROUTER_ADDRESS, amountIn);
-    // by default chainid is not set https://ethereum.stackexchange.com/questions/94412/valueerror-code-32000-message-only-replay-protected-eip-155-transac
-    approveTxUnsigned.chainId = chainId;
-    // estimate gas required to make approve call (not sending it to blockchain either)
-    approveTxUnsigned.gasLimit = await contractIn.estimateGas.approve(V3_SWAP_ROUTER_ADDRESS, amountIn);
-    // suggested gas price (increase if you want faster execution)
-    approveTxUnsigned.gasPrice = await provider.getGasPrice();
-    // nonce is the same as number previous transactions
-    approveTxUnsigned.nonce = await provider.getTransactionCount(walletAddress);
-
-    // sign transaction by our signer
-    const approveTxSigned = await signer.signTransaction(approveTxUnsigned);
-    // submit transaction to blockchain
-    const submittedTx = await provider.sendTransaction(approveTxSigned);
-    // wait till transaction completes
-    const approveReceipt = await submittedTx.wait();
-    if (approveReceipt.status === 0)
-        throw new Error("Approve transaction failed");
-
+        const approveTxSigned = await signer.signTransaction(approveTxUnsigned);
+        const submittedTx = await provider.sendTransaction(approveTxSigned);
+        const approveReceipt = await submittedTx.wait();
+        if (approveReceipt.status === 0)
+            throw new Error("Approve transaction failed");
+    }
 
     console.log("Making a swap...");
-    const value = BigNumber.from(route.methodParameters.value);
+    const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
+    const value = isETH(tokenInContractAddress) ? amountIn : BigNumber.from(route.methodParameters.value);
 
     const transaction = {
         data: route.methodParameters.calldata,
@@ -254,11 +229,8 @@ async function main() {
         value: value,
         from: walletAddress,
         gasPrice: route.gasPriceWei,
-
-        // route.estimatedGasUsed might be too low!
-        // most of swaps I tested fit into 300,000 but for some complex swaps this gas is not enough.
-        // Loot at etherscan/polygonscan past results.
-        gasLimit: BigNumber.from("800000")
+        // gasLimit: BigNumber.from("800_000")
+        gasLimit: BigNumber.from(20_000_000)
     };
 
     const tx = await signer.sendTransaction(transaction);
@@ -267,10 +239,9 @@ async function main() {
         throw new Error("Swap transaction failed");
     }
 
-    // ============= Final part --- printing results
     const [newBalanceIn, newBalanceOut] = await Promise.all([
-        contractIn.balanceOf(walletAddress),
-        contractOut.balanceOf(walletAddress)
+        isETH(tokenInContractAddress) ? provider.getBalance(walletAddress) : tokenInContract.balanceOf(walletAddress),
+        isETH(tokenOutContractAddress) ? provider.getBalance(walletAddress) : tokenOutContract.balanceOf(walletAddress)
     ]);
 
     console.log('');
