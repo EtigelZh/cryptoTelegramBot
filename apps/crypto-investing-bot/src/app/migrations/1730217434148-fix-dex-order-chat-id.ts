d512c09a6693e81e92dd66387a0a94ec8605105d
Andrey Maznyak <andreymaznyak@gmail.com>
2024-11-06 16:19:41 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- apps/crypto-investing-bot/src/app/migrations/1730217434148-fix-dex-order-chat-id.ts
+++ apps/crypto-investing-bot/src/app/migrations/1730217434148-fix-dex-order-chat-id.ts
@@ -4,8 +4,17 @@ export class FixDexOrderChatId1730217434148 implements MigrationInterface {
     name = 'FixDexOrderChatId1730217434148'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Step 1: Create a temporary column to store the current data as text
+        await queryRunner.query(`ALTER TABLE "dex_orders" ADD "chat_dex_order_id_temp" character varying`);
+
+        // Step 2: Copy all data from the original column to the temporary column as strings
+        await queryRunner.query(`UPDATE "dex_orders" SET "chat_dex_order_id_temp" = "chat_dex_order_id"::text`);
+
+        // Step 3: Drop the original column
         await queryRunner.query(`ALTER TABLE "dex_orders" DROP COLUMN "chat_dex_order_id"`);
-        await queryRunner.query(`ALTER TABLE "dex_orders" ADD "chat_dex_order_id" character varying`);
+
+        // Step 4: Rename the temporary column to the original column name
+        await queryRunner.query(`ALTER TABLE "dex_orders" RENAME COLUMN "chat_dex_order_id_temp" TO "chat_dex_order_id"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
